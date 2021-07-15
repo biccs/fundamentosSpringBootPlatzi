e3201962a70601f7dd3cfa086359ad7a08c7e5fb
biccs <biccsdev@gmail.com>
2021-07-15 00:29:11 -0600

    agregando archivos de inicializador spring

---
+++ src/main/java/com/fundamentosPlatzi/springboot/fundamentosPlatzi/FundamentosPlatziApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosPlatzi.springboot.fundamentosPlatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosPlatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosPlatziApplication.class, args);
+	}
+
+}
